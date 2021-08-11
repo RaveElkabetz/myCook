aadba7a604f629e30c2bddfe9ca00c8e78d3cd96
Raveh Elkabetz <rave.elkabetz@gmail.com>
2021-08-11 22:54:32 +0300

    added server.js

--- app.js
+++ app.js
@@ -5,33 +5,14 @@ const app = express();
 cooksRouter = require('./routes/cooksRoutes')
 userRouter = require('./routes/usersRoutes')
 
-
 //MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
-
-
-
-
-
-
-
-
 
 // ROUTES
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/cooks', cooksRouter);
 
-
-//app.get('/api/v1/cooks', getAllCooks);
-//app.get('/api/v1/cooks/:id', getCookById);
-
-//app.post(`/api/v1/cooks`,createNewCook);
+module.exports = app;
 
 
-
-//START THE SERVER
-const port = 3000;
-app.listen(port, () => {
-    console.log(`app running on port ${port}`);
-});
---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+//START THE SERVER
+const port = 3000;
+app.listen(port, () => {
+    console.log(`app running on port ${port}`);
+});
