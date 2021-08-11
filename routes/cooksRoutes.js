603cea0a82eb2199e6f5b8e56fe571153177ce70
Raveh Elkabetz <rave.elkabetz@gmail.com>
2021-08-11 23:37:58 +0300

    added a middleware for checking id, in id router

--- routes/cooksRoutes.js
+++ routes/cooksRoutes.js
@@ -1,20 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const express = require('express');
-const cooksController = require('./../controllers/cooksController');
-
-//ROUTES HANDLING
-
+const express = require("express");
+const cooksController = require("./../controllers/cooksController");
 
 const router = express.Router();
 
-router
-    .route('/')  //the version is for branching out from the current version from the current version without braking v1 for others that use v1
-    .get(cooksController.getAllCooks)
-    .post(cooksController.createNewCook);
-router
-    .route('/:id')
-    .get(cooksController.getCookById);
+router.param("id", cooksController.checkId);
 
+router
+  .route("/") //the version is for branching out from the current version from the current version without braking v1 for others that use v1
+  .get(cooksController.getAllCooks)
+  .post(cooksController.createNewCook);
+router.route("/:id").get(cooksController.getCookById);
 
 module.exports = router;
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,19 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const usersController = require('./../controllers/usersController');
+const usersController = require("./../controllers/usersController");
 
 const router = express.Router();
 
-
 router
-    .route('/')
-    .get(usersController.getAllUsers)
-    .post(usersController.createUser)
+  .route("/")
+  .get(usersController.getAllUsers)
+  .post(usersController.createUser);
 router
-    .route('/:id')
-    .get(usersController.getUserById)
-    .patch(usersController.updateUser)
-    .delete(usersController.deleteUser,router);
-
+  .route("/:id")
+  .get(usersController.getUserById)
+  .patch(usersController.updateUser)
+  .delete(usersController.deleteUser, router);
 
 module.exports = router;
