e51102ba8f3c53fe2eb29091294fcb7808ad7435
RaveElkabetz <48412301+RaveElkabetz@users.noreply.github.com>
2021-09-12 20:39:34 +0300

    added reactive list of ingredients

--- Client/src/pages/AddNewCookPage.vue
+++ Client/src/pages/AddNewCookPage.vue
@@ -1,13 +1,13 @@
 <template>
   <base-layout page-title="הוספת מתכון חדש" PageDefaultBackLink="/categories">
     <ion-item>
-      <ion-label position="floating">שם המתכון</ion-label>
+      <ion-label position="floating" required="true">שם המתכון</ion-label>
       <ion-input></ion-input>
     </ion-item>
 
     <ion-item>
       <ion-label>קטגוריה</ion-label>
-      <ion-select dir="rtl" interface="popover">
+      <ion-select dir="rtl" interface="popover" required="true">
         <ion-select-option value="1">תבשילי קדרה</ion-select-option>
         <ion-select-option value="2">מאפים</ion-select-option>
         <ion-select-option value="3">אוכל אסייתי</ion-select-option>
@@ -16,6 +16,27 @@
         <ion-select-option value="6">סלטים</ion-select-option>
       </ion-select>
     </ion-item>
+    
+        
+    <ion-label>רשימת מצרכים</ion-label>
+  <ion-item>
+      <ion-input ionFocus="changeIsFocused()" placeholder="הוסף כאן מרכיב חדש:  " v-model="newIngredientToAdd" ></ion-input>
+            <ion-buttons>
+        <ion-button style="font-size: 30px;" color="success" v-on:click="addNewIngrClick" >+</ion-button>
+      </ion-buttons>
+      </ion-item>
+      <ion-item v-for="i in this.ingredientsArray" :key="i.id">
+      <ion-input >{{ i }}</ion-input>
+
+      
+      <ion-buttons>
+        <!-- <ion-button color="success" v-if="this.ingredientsArray.length > 0" >+</ion-button> -->
+        <ion-button color="danger" v-if="this.ingredientsArray.length > 0" >x</ion-button>
+      </ion-buttons>
+      </ion-item>
+
+ 
+    
     <ion-item>
       <ion-label position="stacked">תיאור המתכון</ion-label>
       <ion-textarea
@@ -25,7 +46,7 @@
       ></ion-textarea>
     </ion-item>
     <ion-buttons>
-      <ion-button color="success">Success</ion-button>
+      <ion-button class="ion-text-center" v-on:click="checkInput" color="success">הוסף מתכון חדש</ion-button>
     </ion-buttons>
   </base-layout>
 </template>
@@ -51,5 +72,31 @@ export default {
     IonLabel,
     IonItem,
   },
+  data(){
+    return{
+      isFocused: false,
+      ingredientsArray:[],
+      newIngredientToAdd: ""
+    };
+  },
+  methods:{
+    addNewIngrClick(){
+      // console.log(this.newIngredientToAdd);
+      if (this.newIngredientToAdd ==="") {
+        return;
+        
+      }
+      this.ingredientsArray.push(this.newIngredientToAdd);
+      this.newIngredientToAdd = "";
+
+    },
+    checkInput(){
+      console.log("test!");
+    },
+    changeIsFocused(){
+      this.isFocused = true;
+      console.log(this.isFocused);
+    }
+  }
 };
 </script>
