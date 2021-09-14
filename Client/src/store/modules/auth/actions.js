f2f9eeb8e1456ae30d982b246f8ee5486a773292
Raveh Elkabetz <rave.elkabetz@gmail.com>
2021-09-14 17:12:51 +0300

    connected vueX to the auth system2

--- Client/src/store/modules/auth/actions.js
+++ Client/src/store/modules/auth/actions.js
@@ -1,1 +1,24 @@
-export default {};
+export default {
+  login() {},
+  async signup(context, payload) {
+    const response = await fetch("http://localhost:3000/api/test/signup", {
+      method: "POST",
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+      }),
+    });
+    const responseData = await response.json();
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(responseData.message || "Failed to authenticate");
+      throw error;
+    }
+    console.log(responseData);
+    context.commit("setUser", {
+      token: responseData.accessToken,
+      userId: responseData.id,
+      tokenExpiration: 24,
+    });
+  },
+};
---
+++ Client/src/store/modules/auth/getters.js
@@ -0,0 +1,5 @@
+export default {
+  userEmail(state) {
+    return state.userId;
+  },
+};
