858d5089797bd1a68857b2b451d593fd7c2d416e
Raveh Elkabetz <rave.elkabetz@gmail.com>
2021-09-17 09:17:29 +0300

    fixed cook page issue

--- Client/components/cooks/CookPage.vue
+++ Client/components/cooks/CookPage.vue
@@ -1,17 +1,17 @@
 <template>
-  <base-layout :page-title="cook.cookName" PageDefaultBackLink="/categories">
+  <base-layout :page-title="oneCook.cookName" PageDefaultBackLink="/categories">
     <ion-card dir="rtl">
-      <img :src="cook.imageLink" :alt="cook.cookName" />
+      <img :src="oneCook.imageLink" :alt="oneCook.cookName" />
       <ion-card-header>
-        <ion-card-title>{{ cook.cookName }}</ion-card-title>
+        <ion-card-title>{{ oneCook.cookName }}</ion-card-title>
       </ion-card-header>
       <ion-card-content>
         <ul>
-          <li v-for="i in cook.ingredients" :key="i" :vid-id="i">
+          <li v-for="i in oneCook.ingredients" :key="i" :vid-id="i">
             <ion-label>{{ i }} </ion-label>
           </li>
         </ul>
-        {{ cook.fullRecipeDesc }}
+        {{ oneCook.fullRecipeDesc }}
       </ion-card-content>
     </ion-card>
   </base-layout>
@@ -49,10 +49,15 @@ export default {
   data() {
     return {
       cookId: this.$route.params.id,
+      changedFlag: false,
+      Cookt: null,
     };
   },
   methods: {
     init() {
+      if (this.changedFlag === true) {
+        return this.Cookt;
+      }
       //debugger;
       fetch("http://localhost:3000/api/v1/cooks")
         .then((response) => {
@@ -65,20 +70,30 @@ export default {
           //console.log(data.data.cooks);
 
           this.cooksArray = [...data.data.cooks];
+          debugger;
+          for (let index = 0; index < this.cooksArray.length; index++) {
+            const cook = this.cooksArray[index];
+            if (cook.cookName === this.cookId.trim()) {
+              //this.oneCook = this.cooksArray[index];
+              this.changedFlag = true;
+              this.Cookt = this.cooksArray[index];
+              return this.cooksArray[index];
+            }
+          }
         });
 
       //debugger;
 
-      for (let index = 0; index < this.cooksArray.length; index++) {
-        const cook = this.cooksArray[index];
-        if (cook.id === this.cookId) {
-          return cook;
-        }
-      }
+      // for (let index = 0; index < this.cooksArray.length; index++) {
+      //   const cook = this.cooksArray[index];
+      //   if (cook.id === this.cookId) {
+      //     return cook;
+      //   }
+      //}
     },
   },
   computed: {
-    cook() {
+    oneCook() {
       //debugger;
       return this.init();
       //console.log("logging the ingredients");
